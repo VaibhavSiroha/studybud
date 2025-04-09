8b8151f0139de4590679818b1efa2d9737ce1b62
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-09 19:22:06 +0530

    Added a Sql Lite Database

--- base/views.py
+++ base/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Room
+'''
 rooms=[
     {'id':1,'name':'Lets learn python'},
     {'id':2,'name':'Design With Me'},
     {'id':3,'name':'Breaking Bihar'},
 ]
+'''
 
 def home(requset):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(requset,'base/home.html',context)
 
 def room(requset,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room= Room.objects.get(id=pk)
     context={'room': room}
     return render(requset,'base/room.html',context)
