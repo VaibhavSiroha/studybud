c15478e2923b408085d625b0e3f936ceb8b24491
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-09 14:08:51 +0530

    Creating My first Django project

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms=[
+    {'id':1,'name':'Lets learn python'},
+    {'id':2,'name':'Design With Me'},
+    {'id':3,'name':'Breaking Bihar'},
+]
+
+def home(requset):
+    context={'rooms':rooms}
+    return render(requset,'base/home.html',context)
+
+def room(requset,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+    context={'room': room}
+    return render(requset,'base/room.html',context)
