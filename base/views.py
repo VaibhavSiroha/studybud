cd7b529fb64cae43917fb5a71415aee4cb669340
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-10 11:03:57 +0530

    added working forms

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
+from .forms import RoomForm
 '''
 rooms=[
     {'id':1,'name':'Lets learn python'},
@@ -17,3 +18,13 @@ def room(requset,pk):
     room= Room.objects.get(id=pk)
     context={'room': room}
     return render(requset,'base/room.html',context)
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
