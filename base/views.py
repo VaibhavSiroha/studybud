8f4a3b9a1747e3b197bd946316fb84e0155753f6
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-12 12:59:14 +0530

    added error messages and login page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Room,Topic
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from django.db.models import Q
 from .forms import RoomForm
 '''
@@ -10,11 +13,30 @@ rooms=[
 ]
 '''
 
+def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        try:
+            user=User.objects.get(usename=username)
+        except:
+            messages.error(request,'User does not exist')
+
+
+    context={}
+    return render(request,'base/login_register.html',context)
+
 def home(request):
     q=request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms=Room.objects.filter(Q(topic__name__icontains=q)|Q(name__icontains =q)|Q(description__icontains =q))
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=q)|
+        Q(name__icontains =q)
+        |Q(description__icontains =q)
+        )
+    
+    room_count=rooms.count()
     topics=Topic.objects.all()
-    context={'rooms':rooms,'topics':topics}
+    context={'rooms':rooms,'topics':topics,'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(requset,pk):
