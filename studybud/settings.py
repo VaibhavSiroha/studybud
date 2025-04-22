936b14cde7215e7d3e4a8dd3095df4dd6fc2f477
Vaibhav <vaibhavsiroha2000@gmail.com>
2025-04-22 14:32:31 +0530

    Add new image file to static/images directory

--- studybud/settings.py
+++ studybud/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+AUTH_USER_MODEL='base.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -110,11 +112,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL ='/images/'
 
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 
+MEDIA_ROOT= BASE_DIR / 'static/images'
 #STATIC_ROOT =
 
 # Default primary key field type
